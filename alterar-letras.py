1221ef85e5a2b33937e35109877358bf19a4d00c
Jean <jeanvccamargo@gmail.com>
2024-12-09 11:09:17 -0300

    letras-mudança

---
+++ alterar-letras.py
@@ -0,0 +1,6 @@
+def swap_case(s):
+    return ''.join(map(lambda c: c.upper() if c.islower() else c.lower(), s))
+
+if __name__ == '__main__':
+    s = input("Digite uma string: ")
+    print(swap_case(s))
